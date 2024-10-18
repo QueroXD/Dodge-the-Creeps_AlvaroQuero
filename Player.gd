ed1bfd467662eaae87c269ae7f3350fa4c8735d8
QueroXD <alvaroqueroariza@gmail.com>
2024-10-18 16:52:36 +0200

    Assignacion de teclas

--- Player.gd
+++ Player.gd
@@ -1,5 +1,6 @@
 extends Area2D
-
+export var speed = 400 # A quina velocitat es mourà el jugador (píxels/seg).
+var screen_size # Mida de la finestra de joc.
 
 # Declare member variables here. Examples:
 # var a = 2
@@ -8,8 +9,7 @@ extends Area2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	screen_size = get_viewport_rect().size
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
